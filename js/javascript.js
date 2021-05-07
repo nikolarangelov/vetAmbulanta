f44d5264dbc4e36e57c6c880e8d3d3a6c03f099f
nikolarangelov <you@example.com>
2021-05-07 14:56:19 +0200

    Dodate nove sekcije

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,41 @@
+/**LightBox JavaScript */
+
 const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
 const images = document.querySelectorAll('#slika')
 images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('active')
-        const img = document.createElement('img')
-        img.src = image.src
-        while(lightbox.firstChild){
-            lightbox.removeChild(lightbox.firstChild)
-        }
-        lightbox.appendChild(img)
-
-    } )
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
 })
 
-lightbox.addEventListener('click' => {
-    if (e.target !== e.currentTarget) return
-    lightbox.classList.remove('active')
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
 })
+/**End of LightBox JavaScript */
+
+
+
+
+
+
+
+
+/**Responsive NavBar JavaScript */
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
